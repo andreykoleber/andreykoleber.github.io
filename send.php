ccc6f027822e7f440a98d2cb582c7d921dde8fb7
andreykoleber <andrei.koleber@gmail.com>
2018-03-26 22:28:47 +0600

    add new phone service

--- send.php
+++ send.php
@@ -1,6 +1,6 @@
 ﻿<?php
-if((isset($_POST['user-name-input'])&&$_POST['user-name-input']!="")&&(isset($_POST['user-phone-input'])&&$_POST['user-phone-input']!="")){ 
-        $to = 'sklad@9маябизнес.рф'; 
+if((isset($_POST['user-name-input'])&&$_POST['user-name-input']!="")&&(isset($_POST['user-phone-input'])&&$_POST['user-phone-input']!="")){
+        $to = 'info@xn--9-8sbdqjfzm1b0j.xn--p1ai';
         $subject = 'Обратный звонок'; //Загаловок сообщения
         $message = '
                 <html>
@@ -9,16 +9,16 @@ if((isset($_POST['user-name-input'])&&$_POST['user-name-input']!="")&&(isset($_P
                     </head>
                     <body>
                         <p>Имя: '.$_POST['user-name-input'].'</p>
-                        <p>Телефон: '.$_POST['user-phone-input'].'</p>                        
+                        <p>Телефон: '.$_POST['user-phone-input'].'</p>
                     </body>
-                </html>'; 
-        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; 
+                </html>';
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n";
         mail($to, $subject, $message, $headers);
 }
 
-if((isset($_POST['leave-order-name-inpute'])&&$_POST['leave-order-name-inpute']!="")&&(isset($_POST['leave-order-phone-inpute'])&&$_POST['leave-order-phone-inpute']!="")){ 
-        $to = 'sklad@9маябизнес.рф'; 
-        $subject = 'Оставленная заявка'; 
+if((isset($_POST['leave-order-name-inpute'])&&$_POST['leave-order-name-inpute']!="")&&(isset($_POST['leave-order-phone-inpute'])&&$_POST['leave-order-phone-inpute']!="")){
+        $to = 'info@xn--9-8sbdqjfzm1b0j.xn--p1ai';
+        $subject = 'Оставленная заявка';
         $message = '
                 <html>
                     <head>
@@ -26,17 +26,17 @@ if((isset($_POST['leave-order-name-inpute'])&&$_POST['leave-order-name-inpute']!
                     </head>
                     <body>
                         <p>Имя: '.$_POST['leave-order-name-inpute'].'</p>
-                        <p>Телефон: '.$_POST['leave-order-phone-inpute'].'</p>                        
+                        <p>Телефон: '.$_POST['leave-order-phone-inpute'].'</p>
                     </body>
-                </html>'; 
-        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; 
+                </html>';
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n";
         mail($to, $subject, $message, $headers);
 }
 
 
-if((isset($_POST['mini-complement-user-name'])&&$_POST['mini-complement-user-name']!="")&&(isset($_POST['mini-complement-user-phone'])&&$_POST['mini-complement-user-phone']!="")){ 
-        $to = 'sklad@9маябизнес.рф'; 
-        $subject = 'Оставленная заявка на миникомплект'; 
+if((isset($_POST['mini-complement-user-name'])&&$_POST['mini-complement-user-name']!="")&&(isset($_POST['mini-complement-user-phone'])&&$_POST['mini-complement-user-phone']!="")){
+        $to = 'info@xn--9-8sbdqjfzm1b0j.xn--p1ai';
+        $subject = 'Оставленная заявка на миникомплект';
         $message = '
                 <html>
                     <head>
@@ -44,18 +44,18 @@ if((isset($_POST['mini-complement-user-name'])&&$_POST['mini-complement-user-nam
                     </head>
                     <body>
                         <p>Имя: '.$_POST['mini-complement-user-name'].'</p>
-                        <p>Телефон: '.$_POST['mini-complement-user-phone'].'</p>                        
+                        <p>Телефон: '.$_POST['mini-complement-user-phone'].'</p>
                     </body>
-                </html>'; 
-        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; 
+                </html>';
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n";
         mail($to, $subject, $message, $headers);
 }
 
 
 
-if((isset($_POST['standard-complement-user-name'])&&$_POST['standard-complement-user-name']!="")&&(isset($_POST['standard-complement-user-phone'])&&$_POST['standard-complement-user-phone']!="")){ 
-        $to = 'sklad@9маябизнес.рф'; 
-        $subject = 'Оставленная заявка на стандартный'; 
+if((isset($_POST['standard-complement-user-name'])&&$_POST['standard-complement-user-name']!="")&&(isset($_POST['standard-complement-user-phone'])&&$_POST['standard-complement-user-phone']!="")){
+        $to = 'info@xn--9-8sbdqjfzm1b0j.xn--p1ai';
+        $subject = 'Оставленная заявка на стандартный';
         $message = '
                 <html>
                     <head>
@@ -63,18 +63,18 @@ if((isset($_POST['standard-complement-user-name'])&&$_POST['standard-complement-
                     </head>
                     <body>
                         <p>Имя: '.$_POST['standard-complement-user-name'].'</p>
-                        <p>Телефон: '.$_POST['standard-complement-user-phone'].'</p>                        
+                        <p>Телефон: '.$_POST['standard-complement-user-phone'].'</p>
                     </body>
                 </html>';
-        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; 
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n";
         mail($to, $subject, $message, $headers);
 }
 
 
 
-if((isset($_POST['optimal-complement-user-name'])&&$_POST['optimal-complement-user-name']!="")&&(isset($_POST['optimal-complement-user-phone'])&&$_POST['optimal-complement-user-phone']!="")){ 
-        $to = 'sklad@9маябизнес.рф'; 
-        $subject = 'Оставленная заявка на оптимальный комплект'; 
+if((isset($_POST['optimal-complement-user-name'])&&$_POST['optimal-complement-user-name']!="")&&(isset($_POST['optimal-complement-user-phone'])&&$_POST['optimal-complement-user-phone']!="")){
+        $to = 'info@xn--9-8sbdqjfzm1b0j.xn--p1ai';
+        $subject = 'Оставленная заявка на оптимальный комплект';
         $message = '
                 <html>
                     <head>
@@ -82,18 +82,18 @@ if((isset($_POST['optimal-complement-user-name'])&&$_POST['optimal-complement-us
                     </head>
                     <body>
                         <p>Имя: '.$_POST['optimal-complement-user-name'].'</p>
-                        <p>Телефон: '.$_POST['optimal-complement-user-phone'].'</p>                        
+                        <p>Телефон: '.$_POST['optimal-complement-user-phone'].'</p>
                     </body>
                 </html>';
-        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; 
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n";
         mail($to, $subject, $message, $headers);
 }
 
 
 
-if((isset($_POST['huge-complement-user-name'])&&$_POST['huge-complement-user-name']!="")&&(isset($_POST['huge-complement-user-phone'])&&$_POST['huge-complement-user-phone']!="")){ 
-        $to = 'sklad@9маябизнес.рф'; 
-        $subject = 'Оставленная заявка на оптовый комплект'; 
+if((isset($_POST['huge-complement-user-name'])&&$_POST['huge-complement-user-name']!="")&&(isset($_POST['huge-complement-user-phone'])&&$_POST['huge-complement-user-phone']!="")){
+        $to = 'info@xn--9-8sbdqjfzm1b0j.xn--p1ai';
+        $subject = 'Оставленная заявка на оптовый комплект';
         $message = '
                 <html>
                     <head>
@@ -101,9 +101,9 @@ if((isset($_POST['huge-complement-user-name'])&&$_POST['huge-complement-user-nam
                     </head>
                     <body>
                         <p>Имя: '.$_POST['huge-complement-user-name'].'</p>
-                        <p>Телефон: '.$_POST['huge-complement-user-phone'].'</p>                        
+                        <p>Телефон: '.$_POST['huge-complement-user-phone'].'</p>
                     </body>
                 </html>';
-        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; 
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n";
         mail($to, $subject, $message, $headers);
 }
